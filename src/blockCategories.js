af0e99ca258d6532fe10668c382c68c3f90c81e5
Preet P. Vadaliya <preetvadaliya.ict18@gmail.com>
2022-04-02 00:22:17 +0530

    Blocks categories defined.

---
+++ src/blockCategories.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview
+ * This file contains the static literals for blocks category.
+ * @author Preet P. Vadaliya
+ */
+
+Blockly.CATEGORY_CONTROLS = 'Controls';
+Blockly.CATEGORY_LOOPS = 'Loops';
+Blockly.CATEGORY_ARITHMETIC_OPERATORS = 'Arithmetic Operators';
+Blockly.CATEGORY_ASSIGNMENT_OPERATORS = 'Assignment Operators';
+Blockly.CATEGORY_COMPARISON_OPERATORS = 'Comparison Operators';
+Blockly.CATEGORY_LOGICAL_OPERATORS = 'Logical Operators';
+Blockly.CATEGORY_TYPE_OPERATORS = 'Type Operators';
+Blockly.CATEGORY_BITWISE_OPERATORS = 'Bitwise Operators';
+Blockly.CATEGORY_MATHS = 'Maths';
+Blockly.CATEGORY_STRINGS = 'Strings';
+Blockly.CATEGORY_SETS = 'Sets';
+Blockly.CATEGORY_ARRAYS = 'Arrays';
+Blockly.CATEGORY_OBJECTS = 'Objects';
+Blockly.CATEGORY_VARIABLES = 'Variables';
+Blockly.CATEGORY_FUNCTIONS = 'Functions';
